c7c6981d58fd31e59dadd3171ef683714b5d7a1e
CodFriend <furkan@codfriend.com>
2023-06-14 17:45:03 +0300

    Eksik Sayfalar Tasarlanıp Eklendi

--- lib/screens/market.dart
+++ lib/screens/market.dart
@@ -1,7 +1,9 @@
 class Market {
+  final String id;
   final String title;
   final String description;
   final String tag;
+  final String image;
 
-  Market(this.title, this.description, this.tag);
+  Market(this.id, this.title, this.description, this.tag, this.image);
 }
